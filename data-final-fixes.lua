39aee63edb25b88b363bc0f0debfc21aefc690fa
Ben <bolderbrush10@gmail.com>
2021-12-20 09:17:05 -0500

    Fix other mods changing turret health to differ from the health we use

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,27 @@
 -- Be sure to declare functions and vars as 'local' in prototype / data*.lua files,
 -- because other mods may have inadvertent access to functions & variables at this step.
 
-require("sl-defines")
 require("prototypes.sl-entities")
 require("prototypes.sl-techItemRecipe")
+
+local d = require("sl-defines")
+local u = require("sl-util")
+
+
+-- Since some other mods mess with the health of entities 
+-- in a later data stage than they probably should;
+-- try to make sure our copies of their turrets match their health
+local function MatchHealth(currTable)
+  for index, turret in pairs(currTable) do
+    if u.EndsWith(turret.name, d.boostSuffix) then
+	  local originalT = turret.name:sub(1, #turret.name - #d.boostSuffix)
+
+	  turret.max_health = currTable[originalT].max_health
+	end
+  end
+end
+
+
+MatchHealth(data.raw["ammo-turret"])
+MatchHealth(data.raw["fluid-turret"])
+MatchHealth(data.raw["electric-turret"])
--- zPack.sh
+++ zPack.sh
@@ -1,2 +1,2 @@
 export PATH=$PATH:"C:\Program Files\7-Zip"
-7z a "..\SearchlightAssault_1.0.4.zip" ../SearchlightAssault_1.0.4 -r -xr@exclude.txt
+7z a "..\SearchlightAssault_1.0.5.zip" ../SearchlightAssault_1.0.5 -r -xr@exclude.txt
