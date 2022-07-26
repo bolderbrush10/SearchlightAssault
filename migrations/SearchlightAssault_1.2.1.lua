28229d3d85297b7d07f4d87da24f27fb14ee1cb8
Ben <bolderbrush10@gmail.com>
2022-07-26 18:51:53 -0400

    Fix save file migration

--- migrations/SearchlightAssault_1.2.1.lua
+++ migrations/SearchlightAssault_1.2.1.lua
@@ -1,6 +1,8 @@
 -- Since we added hero-turrets as a hidden optional dependency,
 -- force regeneration of the boostInfo table
 
+global.remoteBlock = {}
+
 local cb = require "control-blocklist"
 
 cb.UpdateBlockList()
